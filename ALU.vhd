db59ee729cfc080654476aa4d0606efff4014e1c
Raf Ellinitakis <nickpap556@gmail.com>
2023-03-12 14:15:20 +0200

    Completely wokring and tested ALU

---
+++ ALU.vhd
@@ -0,0 +1,124 @@
+----------------------------------------------------------------------------------
+-- Company: Technical University of Crete
+-- Engineer: Antonios Rafail Ellinitakis
+-- 
+-- Create Date:    23:04:04 04/05/2022 
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+--use IEEE.STD_LOGIC_UNSIGNED.ALL; -- confuses "-","+" with STD_LOGIC_SIGNED.all
+use IEEE.STD_LOGIC_SIGNED.all;
+use ieee.NUMERIC_STD.all;
+
+-- Here are defined the input and output "pins" of our "circuit"
+entity ALU is
+    Port ( A : in  STD_LOGIC_VECTOR (31 downto 0);
+           B : in  STD_LOGIC_VECTOR (31 downto 0);
+           Op : in  STD_LOGIC_VECTOR (3 downto 0);
+           Output : out  STD_LOGIC_VECTOR (31 downto 0);
+           Zero : out  STD_LOGIC;
+           Cout : out  STD_LOGIC;
+           Ovf : out  STD_LOGIC);
+end ALU;
+
+-- Here we can define "wires"(internal signals) running from each "pin" 
+architecture Behavioral of ALU is
+	signal output_wire:STD_LOGIC_VECTOR (31 downto 0);
+	signal output_wire_33_bits:STD_LOGIC_VECTOR (32 downto 0);
+	signal zero_flag_wire:STD_LOGIC := '0';  -- the default value is zero(?)
+	signal ovf_wire:STD_LOGIC := '0';
+	signal cout_wire:STD_LOGIC := '0';
+
+--> variables in processes are like "local" variables in functions
+--> they are assigned with ":="
+--> they are defined between the "process" and "begin" keywords
+--> they always get immediately the value that is assigned to them.
+
+--> signals are like global variables 
+--> they are assigned with "<="
+--> Signals are defined in the architecture, before the begin keyword.
+--> they do not always get immediately the change applied to them
+--> (in combinatorial code, they get the values immediately. In sequential they wait for certain events)
+
+begin
+	-- the process is a behavioral description of the circuit
+	-- a process is executed sequentially, but different processes are executed concurrently!
+	process(A,B, Op)  -- inside the parenthes(e)is, there is the sensitivity list of signals.Changes to these signals, trigger this process to execute.
+	begin
+		case(Op) is  -- the case statement is like a MUXer with the "Op" signal as input
+			-- Arithmetic Addition
+			when "0000" => output_wire <= A+B;
+			-- Arithmetic Subtraction
+			when "0001" => output_wire <= A-B;
+			-- Logical AND
+			when "0010" => output_wire <= A and B;  -- logical operators in VHDL are called by their name
+			-- Logical OR
+			when "0011" => output_wire <= A or B;
+			-- Inversion of A
+			when "0100" => output_wire <= not A;
+			-- Numerical shift Right
+			when "1000" =>
+				output_wire <= std_logic_vector(shift_right(signed(A), 1));
+			--Logical shift Right
+			when "1001" =>
+				output_wire <= std_logic_vector(shift_right(unsigned(A), 1));  -- unsigned because it is a logical shift
+			-- Numerical Shift Left
+			when "1010" =>
+				output_wire <= std_logic_vector(shift_left(signed(A), 1));
+			-- Rotate Left by 1
+			when "1100" =>
+				output_wire <=  std_logic_vector(signed(A) rol 1);
+			-- Rotate Right by 1
+			when "1101" =>
+				output_wire <= std_logic_vector(signed(A) ror 1);
+			when others => 
+				output_wire <= "00000000000000000000000000000000";
+		end case;
+				
+	end process;
+	
+	-- Cout, Zero and OVF Flags control(simultaneously with the above, hence the different process)
+	process(A,B,Op,output_wire)
+	begin
+		--Check for zero output
+		if(output_wire = x"00000000") then 	--zero flag
+			zero_flag_wire <= '1';  -- set the Zero Flag
+		else 
+			zero_flag_wire <= '0';
+		end if;
+		
+		-- Check for Ovf
+		-- In Addition of signed numbers, positive + positive must be positive, and negative + negative must be negative
+		if ((A(31) = B(31)) and (A(31) /= output_wire(31)) and (Op = "0000")) then
+			ovf_wire <= '1';
+		-- In subtraction, the opposite is true.
+		elsif ((A(31) /= B(31)) and (A(31) /= output_wire(31)) and (Op = "0001")) then
+			ovf_wire <= '1';
+		else 
+			ovf_wire <= '0';
+		end if;
+		
+		-- Check for a Carry Out
+		if (Op="0001") then
+			output_wire_33_bits <= (A(31) & A) - (B(31) & B);
+			cout_wire <= output_wire_33_bits(32);  -- if there is a 33rd bit produced, send it as a Cout flag
+		elsif (Op="0000") then
+			output_wire_33_bits <= (A(31) & A) + (B(31) & B);
+			cout_wire <= output_wire_33_bits(32); 
+		else
+			cout_wire <= '0';
+		end if;
+			
+	end process;
+				
+			
+
+	-- Create the logic of the output signals:
+	
+	Output <= output_wire after 10ns; -- Send the result to the output of the ALU
+	Cout <= cout_wire after 10 ns;
+	Ovf <= ovf_wire after 10ns;
+	Zero <= zero_flag_wire after 10ns;
+	
+end Behavioral;
+
---
+++ ALU_tb.vhd
@@ -0,0 +1,154 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+-- Create Date:   20:53:07 04/08/2022
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY ALU_tb IS
+END ALU_tb;
+ 
+ARCHITECTURE behavior OF ALU_tb IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT ALU
+    PORT(
+         A : IN  std_logic_vector(31 downto 0);
+         B : IN  std_logic_vector(31 downto 0);
+         Op : IN  std_logic_vector(3 downto 0);
+         Output : OUT  std_logic_vector(31 downto 0);
+         Zero : OUT  std_logic;
+         Cout : OUT  std_logic;
+         Ovf : OUT  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal A : std_logic_vector(31 downto 0) := (others => '0');
+   signal B : std_logic_vector(31 downto 0) := (others => '0');
+   signal Op : std_logic_vector(3 downto 0) := (others => '0');
+
+ 	--Outputs
+   signal Output : std_logic_vector(31 downto 0);
+   signal Zero : std_logic;
+   signal Cout : std_logic;
+   signal Ovf : std_logic;
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
+ 
+   --  --constant <clock>_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: ALU PORT MAP (
+          A => A,
+          B => B,
+          Op => Op,
+          Output => Output,
+          Zero => Zero,
+          Cout => Cout,
+          Ovf => Ovf
+        );
+
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+		-- Addition and Zero Flag
+		Op <= "0000";
+		A <=  x"00000000";
+		B <=  x"00000000";
+		wait for 50 ns;	
+		
+		-- Subtraction
+		Op <= "0001";
+		A <=  x"0000000f";
+		B <=  x"00000001";
+		wait for 50 ns;
+		
+		-- Carry and OVF at addition
+		Op <= "0000";
+		A <=  x"80000000";
+		B <=  x"ffffffff";
+		wait for 50 ns;
+		
+		-- OVF at addition
+		Op <= "0000";
+		A <=  x"7fffffff";
+		B <=  x"00000001";
+		wait for 50 ns;		
+		
+		-- OVF at subtraction
+		Op <= "0001";
+		A <=  x"7fffffff";
+		B <=  x"ffffffff";
+		wait for 50 ns;
+		
+		-- Carry at subtraction
+		Op <= "0001";
+		A <=  x"fffffffe";
+		B <=  x"ffffffff";
+		wait for 50 ns;
+		
+		-- Simple Addition
+		Op <= "0000";
+		A <=  x"00000008";
+		B <=  x"00000009";
+		wait for 50 ns;
+		
+		-- Numerical shift right
+		Op <= "1000";
+		A <=  x"f0000008";
+		wait for 50 ns;
+		
+		-- Logical shift right
+		Op <= "1001";
+		A <=  x"f0000002";
+		wait for 50 ns;
+		
+		-- Logical shift left
+		Op <= "1010";
+		A <=  x"0000000f";
+		wait for 50 ns;
+		
+		-- Rotate left
+		Op <= "1100";
+		A <=  x"f000000e";
+		wait for 50 ns;
+		
+		-- Rotate right
+		Op <= "1101";
+		A <=  x"e000000f";
+		wait for 50 ns;
+		
+		-- AND
+		Op <= "0010";
+		A <=  x"00000fff";
+		B <=  x"0e00000e";
+		wait for 50 ns;
+		
+		-- OR
+		Op <= "0011";
+		A <=  x"00000fff";
+		B <=  x"0e00000e";
+		wait for 50 ns;
+		
+		-- Not
+		Op <= "0100";
+		A <=  x"0f0b0802";
+		wait for 50 ns;
+		
+		
+
+      wait;
+   end process;
+
+END;
